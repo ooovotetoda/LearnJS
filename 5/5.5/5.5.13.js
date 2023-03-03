eabdd9cb43e2767f836ed7e142f5402670f247c0
ooovotetoda <dima.rolick22@gmail.com>
2023-03-03 14:43:01 +0300

    Второй вариант решения

--- 5/5.5/5.5.13.js
+++ 5/5.5/5.5.13.js
@@ -1,3 +1,4 @@
+/*
 function groupById(array) {
     return array.reduce((obj, value) => {
         obj[value.id] = value;
@@ -12,3 +13,23 @@ let users = [
 ];
 
 let usersById = groupById(users);
+*/
+
+function groupById(arr) {
+    return arr.reduce((acc, obj) => {
+        return {...acc, [obj.id]: obj}
+    }, {})
+
+}
+
+let users = [
+    {id: 'john', name: "John Smith", age: 20},
+    {id: 'ann', name: "Ann Smith", age: 24},
+    {id: 'pete', name: "Pete Peterson", age: 31},
+];
+
+let usersById = groupById(users, "id");
+
+
+
+console.log(usersById)
